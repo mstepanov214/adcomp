4058291be22e9b2815881e8ae46c08aebfe18da3
mstepanov214 <22296883+mstepanov214@users.noreply.github.com>
2025-06-27 23:40:52 +0300

    Refactoring
    - Refactor code
    - Fix empty component name validation
    - Fix existing component creation for Windows

--- src/extension.ts
+++ src/extension.ts
@@ -1,70 +1,94 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
 import * as jsConvert from 'js-convert-case';
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     'adcomp.createAngularDartComponent',
-    async (args: { path: string }) => {
-      const name = await vscode.window.showInputBox({
-        placeHolder: 'Enter the name of the component directory...',
-        validateInput: (value: string) => {
-          if (jsConvert.toSnakeCase(value) !== value) {
-            return 'The component directory name must be in snake_case format';
-          }
-          return null;
-        },
-      });
-      if (name === undefined) {
-        return;
-      }
-      const config = vscode.workspace.getConfiguration();
-      const styleExtension = config.get<string>('adcomp.styleExtension');
-      const includeComments = config.get<boolean>('adcomp.includeComments');
-      
-      const dartFileUri = vscode.Uri.file(path.join(args.path, name, `${name}.dart`));
-      const fileUris = [
-        dartFileUri,
-        vscode.Uri.file(path.join(args.path, name, `${name}.html`)),
-        vscode.Uri.file(path.join(args.path, name, `${name}.${styleExtension}`)),
-      ];
-      try {
-        await vscode.workspace.fs.stat(vscode.Uri.parse(path.join(args.path, name)));
-        return vscode.window.showWarningMessage(`${name} directory already exists`);
-      } catch {
-        const wsedit = new vscode.WorkspaceEdit();
-        for (const fileUri of fileUris) {
-          wsedit.createFile(fileUri);
-        }
-        await vscode.workspace.applyEdit(wsedit);
-
-        const textDocument = await vscode.workspace.openTextDocument(dartFileUri);
-        const editor = await vscode.window.showTextDocument(textDocument);
-        await editor.edit((editBuilder) => {
-          editBuilder.insert(editor.selection.active, "import 'package:ngdart/angular.dart';\n\n");
-        });
-        const selector = jsConvert.toKebabCase(name);
-        const className = jsConvert.toPascalCase(name);
-        const adCompSnippet = new vscode.SnippetString(
-          [
-            ...(includeComments ? ['/// $1'] : []),
-            '@Component(',
-            `\tselector: '${selector}',`,
-            `\ttemplateUrl: '${name}.html',`,
-            `\tstyleUrls: ['${name}.css'],`,
-            '\tdirectives: [coreDirectives],',
-            '\tproviders: [],',
-            ')',
-            `class ${className} {`,
-            includeComments ? '\t$2' : '\t$1',
-            '}',
-          ].join('\n')
-        );
-        editor.insertSnippet(adCompSnippet);
-      }
-    }
+    createAngularDartComponent
   );
   context.subscriptions.push(disposable);
+}
+
+async function createAngularDartComponent({ fsPath }: { fsPath: string }) {
+  const inputBoxOptions: vscode.InputBoxOptions = {
+    placeHolder: 'Enter the name of the component directory...',
+    prompt: `AngularDart component will be created at ${fsPath}`,
+    validateInput: validateInput,
+    ignoreFocusOut: true,
+  };
+  const name = await vscode.window.showInputBox(inputBoxOptions);
+  if (name === undefined) {
+    return;
+  }
+  const config = vscode.workspace.getConfiguration();
+  const styleExtension = config.get<string>('adcomp.styleExtension');
+  const includeComments = config.get<boolean>('adcomp.includeComments');
+  const componentDirectoryUri = vscode.Uri.joinPath(vscode.Uri.file(fsPath), name);
+
+  if (await uriExists(componentDirectoryUri)) {
+    return vscode.window.showErrorMessage(`${componentDirectoryUri.fsPath} already exists.`);
+  }
+
+  const dartFileUri = vscode.Uri.joinPath(componentDirectoryUri, `${name}.dart`);
+  const fileUris = [
+    dartFileUri,
+    vscode.Uri.joinPath(componentDirectoryUri, `${name}.html`),
+    vscode.Uri.joinPath(componentDirectoryUri, `${name}.${styleExtension}`),
+  ];
+
+  try {
+    const wsedit = new vscode.WorkspaceEdit();
+    for (const uri of fileUris) {
+      wsedit.createFile(uri);
+    }
+    await vscode.workspace.applyEdit(wsedit);
+
+    const textDocument = await vscode.workspace.openTextDocument(dartFileUri);
+    const editor = await vscode.window.showTextDocument(textDocument);
+    await editor.edit((editBuilder) => {
+      editBuilder.insert(editor.selection.active, "import 'package:ngdart/angular.dart';\n\n");
+    });
+    const selector = jsConvert.toKebabCase(name);
+    const className = jsConvert.toPascalCase(name);
+    const adCompSnippet = new vscode.SnippetString(
+      [
+        ...(includeComments ? ['/// $1'] : []),
+        '@Component(',
+        `\tselector: '${selector}',`,
+        `\ttemplateUrl: '${name}.html',`,
+        `\tstyleUrls: ['${name}.css'],`,
+        '\tdirectives: [coreDirectives],',
+        '\tproviders: [],',
+        ')',
+        `class ${className} {`,
+        includeComments ? '\t$2' : '\t$1',
+        '}',
+      ].join('\n')
+    );
+    editor.insertSnippet(adCompSnippet);
+  } catch (error) {
+    console.error('Component creation error:', error);
+    vscode.window.showErrorMessage(`Component creation error: ${error}`);
+  }
+}
+
+function validateInput(value: string): string | null {
+  if (!value || value === '') {
+    return 'The component directory name can not be empty';
+  }
+  if (jsConvert.toSnakeCase(value) !== value) {
+    return 'The component directory name must be in snake_case format';
+  }
+  return null;
+}
+
+async function uriExists(uri: vscode.Uri): Promise<boolean> {
+  try {
+    await vscode.workspace.fs.stat(uri);
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 export function deactivate() {}
